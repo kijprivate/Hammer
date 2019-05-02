fadc10413e53a82e59f1a36f599c5011507c337f
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2019-05-02 20:55:21 +0200

    optimized blur, optimization to 60fps

---
+++ Hammer/Assets/FpsCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsCounter : MonoBehaviour
+{
+    public int avgFrameRate;
+    public Text display_Text;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+    }
+}
+
---
+++ Hammer/Assets/_Game/Scripts/BlurFade.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+public class BlurFade : MonoBehaviour
+{
+    [SerializeField] float fadeSpeed =2f;
+
+    private BlurOptimized blur;
+
+    private void Awake()
+    {
+        blur = GetComponent<BlurOptimized>();
+        
+        if(LevelContainer.MenuHided)
+        { blur.enabled = false; }
+
+        EventManager.EventMenuHided += OnMenuHided;
+    }
+
+    private void OnMenuHided()
+    {
+        StartCoroutine(FadeBlur());
+    }
+
+    private IEnumerator FadeBlur()
+    {
+        while (blur.blurSize > Mathf.Epsilon)
+        {
+            yield return new WaitForEndOfFrame();
+            blur.blurSize -= Time.deltaTime*fadeSpeed;
+        }
+        blur.enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.EventMenuHided -= OnMenuHided;
+    }
+}
